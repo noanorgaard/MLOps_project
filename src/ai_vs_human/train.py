02526337966087353971243c1dd8371cdaa46d8b
noanorgaard <anna.noa@gmail.com>
2026-01-09 10:17:52 +0100

    first commit

---
+++ src/ai_vs_human/train.py
@@ -0,0 +1,10 @@
+from ai_vs_human.model import Model
+from ai_vs_human.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
