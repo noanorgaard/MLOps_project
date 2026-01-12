9d97ab8dac7270fe45622d371199febc5924ef4a
Mads <madsgroenbeck@gmail.com>
2026-01-12 11:22:53 +0100

    s

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,3 +1,4 @@
+import torch
 from torch.utils.data import Dataset
 
 from src.ai_vs_human.data import MyDataset
@@ -5,5 +6,5 @@ from src.ai_vs_human.data import MyDataset
 
 def test_my_dataset():
     """Test the MyDataset class."""
-    dataset = MyDataset("data/raw")
+    dataset = MyDataset()
     assert isinstance(dataset, Dataset)
